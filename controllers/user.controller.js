99c9813da0ef9cc165ec5513aef9f91cf9388eea
Kaverlio <nikolaysuslp0@gmail.com>
2021-12-05 16:36:00 +0200

    6

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,22 +1,30 @@
-class UserController{
-    async createUser(req, res){
-        const {name, surname} = req.body;
-        const newPerson = await db.query(`INSERT INTO person (name, surname) values (${name}, ${surname}) RETURNING *`);
-        res.json(newPerson.rows[0]);
-        
+class UserController {
+    async createUser(req, res) {
+        try {
+            const {name, surname} = req.body;
+            const newPerson = await db.query(`INSERT INTO person (name, surname) values (${name}, ${surname}) RETURNING *`);
+            res.json(newPerson.rows[0]);
+    
+        } catch(err){
+            console.log(err);
+        }        
     }
-    async getUsers(req, res){
-        const users = await db.query('SELECT * FROM person');
-        res.json(users.rows);
+    async getUsers(req, res) {
+        try{
+            const users = await db.query('SELECT * FROM person');
+            res.json(users.rows);
+        } catch(err){
+            console.log(err);
+        }
     }
-    async getOneUser(req, res){
-        
+    async getOneUser(req, res) {
+
     }
-    async updateUser(req, res){
-        
+    async updateUser(req, res) {
+
     }
-    async deleteUser(req, res){
-        
+    async deleteUser(req, res) {
+
     }
 }
 
