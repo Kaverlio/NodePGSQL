67d64a5ae0fd46ebdbde94d7b53031608ad5cbc0
Kaverlio <nikolaysuslp0@gmail.com>
2021-12-05 14:55:02 +0200

    3

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || config.get('serverPort') || 8000;
 
 const app = express();
 
+app.use('/', "OK");
+
 app.use('/api', userRouter);
 
 app.listen(PORT, () => console.log(`Server listen port ${PORT}`));
